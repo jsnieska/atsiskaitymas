3d85ac52cb54e6e6ef890389591c65bf330829af
jsnieska <snieska.juozas@gmail.com>
2022-03-13 21:57:23 +0200

    j

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+"use strict";
 
+
+// Sticky Header 
+
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
