07aab01b6b95c1e048cd9c9e34aec9646e8e66fa
jsnieska <snieska.juozas@gmail.com>
2022-03-14 14:49:07 +0200

    j

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 "use strict";
 
+var firstname = document.getElementById("first.name");
+var lastname = document.getElementById("last.name");
+var phone = document.getElementById("phone.number");
+var button1 = document.getElementById("button1");
+var fbutton = document.getElementById("first.button");
+
+
+
+// while (1000){
+// }
+  
+
+  // if(document.getElementById('first-button').clicked == true)
+  // {
+  //   alert("button was");
+  // }
+
+// if(firstname != null && firstname.value == '') {
+//   alert("sveiki");
+// }
 
 // Sticky Header 
-
-
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
@@ -16,3 +34,16 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+fbutton.addEventListener("click", myFunction2);
+ function myFunction2()
+ {
+  alert("Downloaded");
+}
+button1.addEventListener("click", myFunction3);
+ function myFunction3(){
+  if( firstname.value == '' || lastname.value == '' || phone.value == '') {
+      alert("Neuzpildei");
+    }
+ }
