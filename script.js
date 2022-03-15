d550e474c2f0873f60af518f906403e20918e3c1
jsnieska <snieska.juozas@gmail.com>
2022-03-15 17:50:50 +0200

    j

--- script.js
+++ script.js
@@ -3,23 +3,15 @@
 var firstname = document.getElementById("first.name");
 var lastname = document.getElementById("last.name");
 var phone = document.getElementById("phone.number");
+var firstname1 = document.getElementById("first.name1");
+var lastname1 = document.getElementById("last.name2");
+var phone1 = document.getElementById("phone.number3");
 var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
 var fbutton = document.getElementById("first.button");
 
 
 
-// while (1000){
-// }
-  
-
-  // if(document.getElementById('first-button').clicked == true)
-  // {
-  //   alert("button was");
-  // }
-
-// if(firstname != null && firstname.value == '') {
-//   alert("sveiki");
-// }
 
 // Sticky Header 
 window.onscroll = function() {myFunction()};
@@ -36,14 +28,52 @@ function myFunction() {
 }
 
 
+// Forms 
 fbutton.addEventListener("click", myFunction2);
  function myFunction2()
  {
-  alert("Downloaded");
+  alert("Atsiųsta");
 }
 button1.addEventListener("click", myFunction3);
  function myFunction3(){
   if( firstname.value == '' || lastname.value == '' || phone.value == '') {
-      alert("Neuzpildei");
+      alert("Neužpildei");
+    }else {
+      alert("Ačiū!")
     }
  }
+
+
+button2.addEventListener("click", myFunction4);
+ function myFunction4(){
+  if( firstname1.value == '' || lastname1.value == '' || phone1.value == '') {
+      alert("Neužpildei");
+    } else {
+      alert("Ačiū!")
+    }
+ }
+
+
+//  Swipwe
+
+ const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
